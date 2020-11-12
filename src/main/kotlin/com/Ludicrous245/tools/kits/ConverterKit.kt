4c6d0afd7e4caba30f79f23320e1e0beab5515ae
Ludicrous245 <43002691+Ludicrous245@users.noreply.github.com>
2020-11-12 22:18:34 +0900

    1.7d2

--- src/main/kotlin/com/Ludicrous245/tools/kits/ConverterKit.kt
+++
@@ -1,9 +0,0 @@
-package com.Ludicrous245.tools.kits
-
-class ConverterKit {
-    companion object{
-        fun convertID(rawID: String): String {
-            return rawID.replace("<", "").replace("@", "").replace("!", "").replace(">", "")
-        }
-    }
-}
